d43093becaf5ec27ca77f95eb9649c0e7af5e2c5
HienXuanPham <44250274+HienXuanPham@users.noreply.github.com>
2024-01-13 00:01:35 -0500

    Navbar (#2)
    * navbar...
    
    * added navbar-start...
    
    * added images...

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { auth, userData } from '$lib/firebase'
+  import { signOut } from 'firebase/auth'
+
+  async function signOutSSR() {
+    const res = await fetch('/api/signin', { method: 'DELETE' })
+    await signOut(auth)
+  }
+</script>
+
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <span class="hidden lg:inline">Branches</span>
+  </div>
+  <div class="navbar-center">
+    <span class="btn btn-ghost normal-case text-xl">
+      <a href="/{$userData?.username}"
+        >👁️ <span class="hidden lg:inline">View</span></a
+      >
+    </span>
+    <span class="btn btn-ghost normal-case text-xl">
+      <a href="/{$userData?.username}/edit"
+        >🔗 <span class="hidden lg:inline">Edit</span></a
+      >
+    </span>
+    <span class="btn btn-ghost normal-case text-xl">
+      <a href="/{$userData?.username}/bio"
+        >🍪 <span class="hidden lg:inline">Edit Bio</span></a
+      >
+    </span>
+  </div>
+  <div class="navbar-end">
+    <a href="/" class="btn btn-outline btn-xs" on:click={() => signOutSSR()}
+      >Sign Out</a
+    >
+  </div>
+</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { auth, user } from '$lib/firebase'
-  import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+  import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 
   async function signInWithGoogle() {
     const provider = new GoogleAuthProvider()
@@ -17,11 +17,6 @@
       body: JSON.stringify({ idToken }),
     })
   }
-
-  async function signOutSSR() {
-    const res = await fetch('/api/signin', { method: 'DELETE' })
-    await signOut(auth)
-  }
 </script>
 
 <h2>Login</h2>
@@ -29,8 +24,6 @@
 {#if $user}
   <h2 class="card-title">Welcome, {$user.displayName}</h2>
   <p class="text-center text-success">You are logged in</p>
-  <button class="btn btn-warning" on:click={() => signOutSSR()}>Sign out</button
-  >
   <a class="btn btn-primary" href="/login/username">Pick your username</a>
 {:else}
   <button class="btn btn-primary" on:click={signInWithGoogle}
